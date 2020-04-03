09bdbfc2fd2f3d3881a8d62502f1acdc0d91d7ec
Alex Barkin <alexbarkin@rogers.com>
2020-04-03 16:02:22 -0400

    Inital set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+import ScrollAnimation from "react-animate-on-scroll"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Header from "../components/Header"
+import Map1 from "../images/map1.jpeg"
+import Map2 from "../images/map2.jpeg"
+import Map3 from "../images/map3.jpeg"
+import Map4 from "../images/map4.jpeg"
+import Map5 from "../images/map5.jpeg"
+import Map6 from "../images/map6.jpeg"
+import Map7 from "../images/map7.jpeg"
 import SEO from "../components/seo"
 
+import "../style/app.css";
+import "animate.css/animate.min.css";
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <body>
+    <Header/>
+    <SEO title="Map the system AI" />
+      <img alt="IMG" src={Map1} className="map"/>
+    <ScrollAnimation animateIn='bounceInRight'>
+        <img alt="IMG" src={Map2} className="map"/>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn='bounceInRight'>
+        <img alt="IMG" src={Map3} className="map"/>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn='bounceInRight'>
+        <img alt="IMG" src={Map4} className="map"/>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn='bounceInRight'>
+        <img alt="IMG" src={Map5} className="map"/>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn='bounceInRight'>
+        <img alt="IMG" src={Map6} className="map"/>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn='bounceInRight'>
+        <img alt="IMG" src={Map7} className="map"/>
+    </ScrollAnimation>
+  </body>
 )
 
 export default IndexPage
